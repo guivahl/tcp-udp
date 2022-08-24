7e59033b255773f1f2ad8b011c2bd166db52b6ef
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2022-08-24 15:04:02 -0300

    feat: set upper and lower server input messages

--- upper_case_server_UDP.py
+++ upper_case_server_UDP.py
@@ -9,17 +9,34 @@
 
 from socket import *
 
+
+
+def messageService (message):
+	headerLength = 3 # length of 'CA:' and 'CB:' keys
+
+	if message.startswith("CA:"):
+		payload = message[headerLength:]
+		return payload.lower()
+
+	if message.startswith("CB:"):
+		payload = message[headerLength:]
+		return payload.upper()
+	
+	return message
+
 serverIP = 'localhost'  # localhost or your server IP address
 serverPort = 9999		# use the port number you wish (higher than 1023)
 
 serverSocket = socket(AF_INET,SOCK_DGRAM)	# creates a socket (server side)
 serverSocket.bind((serverIP, serverPort))	# bind() associates the socket with its local address [bind() is used in the server side]
 
-print("Server is on!")
+print("Server {} running on port {}!".format(serverIP, serverPort))
 
 while 1:
 	message, clientIP = serverSocket.recvfrom(1500)		# 1500 bytes are read from the UDP socket
 	decodedMessage = message.decode()
-	modifiedMessage = decodedMessage.upper()
-	encodedMessage = modifiedMessage.encode()
+	
+	responseMessage = messageService(decodedMessage)
+
+	encodedMessage = responseMessage.encode()
 	serverSocket.sendto(encodedMessage, clientIP)		# sends converted (upper-case) sentence
